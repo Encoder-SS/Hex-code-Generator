d64c2a80a1fbf6aa82acde68a14e6c982fca8633
Encoder-SS <75261080+Encoder-SS@users.noreply.github.com>
2020-12-04 16:11:34 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,30 @@
+
+
+var clickMe = document.querySelector('button');
+var bcg = document.querySelector('body');
+var hexText = document.querySelector('span');
+
+clickMe.addEventListener('click', hexaFunction);
+
+function hexaFunction(){
+    var colors = ["0","1","2","3","4","5","6","7","8","9","A", "B", "C", "D", "E", "F"];
+    var hexcode = '';
+    for(let i = 0; i<6; i++){
+        let random = Math.floor(Math.random()*colors.length);
+        hexcode += colors[random];
+    }
+    // bcg.style.backgr+0oundColor = randomText;
+    document.body.style.backgroundColor = '#'  +hexcode;
+    document.getElementById("hex-code").innerHTML = hexcode;
+}
+
+// function hexaFunction(){
+//     var colors = ["0","1","2","3","4","5","6","7","8","9","A", "B", "C", "D", "E", "F"];
+//     var hexcode = '';
+//     for(var i = 0; i<6; i++){
+//         var random_value = Math.floor(Math.random()*colors.length);
+//         hexcode += colors[random_value];
+//     }
+//     document.getElementById("hex-code").innerHTML = hexcode;
+//     document.body.style.backgroundColor = '#' + hexcode;
+// }
